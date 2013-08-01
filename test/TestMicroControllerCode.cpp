10283047990231f46d57b251637b04db6ebe678e
James W Grenning <james@grenning.net>
2013-08-01 12:21:51 -0500

    Refactor the tests, extract helper functions to cut down on the noise.

--- test/TestMicroControllerCode.cpp
+++ test/TestMicroControllerCode.cpp
@@ -19,30 +19,32 @@ TEST_GROUP(MicroControllerCode)
 		mock("register").checkExpectations();
 		mock().clear();
 	}
+
+	void expect_write_reg(const char * reg, int value)
+	{
+		mock("register")
+			.expectOneCall("WRITE_REG")
+			.withParameter("reg", reg)
+			.withParameter("data", value);
+	}
+
+	void expect_read_reg(const char * reg, int returing_value)
+	{
+		mock("register")
+			.expectOneCall("READ_REG")
+			.withParameter("reg", reg)
+			.andReturnValue(returing_value);
+	}
 };
+
 
 TEST(MicroControllerCode, say_hi)
 {
-	mock("register")
-			.expectOneCall("WRITE_REG")
-			.withParameter("reg", "IE")
-			.withParameter("data", b11000000);
-	mock("register")
-			.expectOneCall("READ_REG")
-			.withParameter("reg", "TI_0")
-			.andReturnValue(42);
-	mock("register")
-			.expectOneCall("READ_REG")
-			.withParameter("reg", "TI_0")
-			.andReturnValue(42);
-	mock("register")
-			.expectOneCall("READ_REG")
-			.withParameter("reg", "TI_0")
-			.andReturnValue(42);
-	mock("register")
-			.expectOneCall("READ_REG")
-			.withParameter("reg", "TI_0")
-			.andReturnValue(42);
+	expect_write_reg("IE", b11000000);
+	expect_read_reg("TI_0", 42);
+	expect_read_reg("TI_0", 42);
+	expect_read_reg("TI_0", 42);
+	expect_read_reg("TI_0", 42);
 
 	say_hi();
 }
